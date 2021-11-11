ea82841e6c66fec566901c262bf4cc66c00944bf
Evgeniy Trofimov <evgenii.trofimov@gett.com>
2021-11-11 22:56:43 +0300

    auth service copied from sample repo

---
+++ auth/app/models/account.rb
@@ -0,0 +1,42 @@
+class Account < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :access_grants,
+           class_name: 'Doorkeeper::AccessGrant',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
+  has_many :access_tokens,
+           class_name: 'Doorkeeper::AccessToken',
+           foreign_key: :resource_owner_id,
+           dependent: :delete_all # or :destroy if you need callbacks
+
+  enum role: {
+    admin: 'admin',
+    accounting_clerk: 'accounting_clerk',
+    repairman: 'repairman',
+    employee: 'employee'
+  }
+
+  after_create do
+    account = self
+
+    # ----------------------------- produce event -----------------------
+    event = {
+      event_name: 'AccountCreated',
+      data: {
+        public_id: account.public_id,
+        email: account.email,
+        full_name: account.full_name,
+        position: account.position,
+        role: account.role
+      }
+    }
+
+    WaterDrop::SyncProducer.call(event.to_json, topic: 'accounts-stream')
+    # --------------------------------------------------------------------
+  end
+end
